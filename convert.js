2f7e6d361fda2b89c4ac7ada689167ec72f79324
Nguyen Thang <thang.nd0226@gmail.com>
2024-09-23 21:35:17 +0700

    add files

---
+++ convert.js
@@ -0,0 +1,22 @@
+function footToMeter(foot) {
+  let meter = 0.305 * foot;
+  return meter;
+}
+
+function meterToFoot(meter) {
+  let foot = 3.279 * meter;
+  return foot;
+}
+
+let feet_arr = [1.0, 2.0, 3.0, 4.0, 5.0, 6.0, 7.0, 8.0, 9.0, 10.0];
+let meter_arr = new Array(10);
+for (ind in feet_arr) {
+  meter_arr[ind] = footToMeter(feet_arr[ind]);
+}
+console.log(meter_arr);
+
+let feet_arr2 = new Array(10);
+for (ind in meter_arr) {
+  feet_arr2[ind] = meterToFoot(meter_arr[ind]);
+}
+console.log(feet_arr2);
